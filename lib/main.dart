33d0cf8f8f03c4d4545766aa2fe5ad853d4e519f
Arthur Hoengen <arthurhoengen@gmail.com>
2023-10-23 20:48:34 -0300

    Fazendo o setup do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:chat_flow/pages/chat.dart';
 import 'package:chat_flow/pages/menu.dart';
 import 'package:chat_flow/pages/profile.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_flow/pages/login.dart';
 import 'package:chat_flow/pages/register.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
